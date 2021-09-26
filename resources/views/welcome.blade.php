96ac8c03bd8fd1eb7b5d69389de9f2d3b9d87638
Farhan Hasin Chowdhury <shovik.is.here@gmail.com>
2021-09-26 19:01:09 +0600

    Added vendor extraction

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,11 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel React</title>
-
-    <script src="{{ asset('js/app.js') }}" defer></script>
 </head>
 <body>
     <div id="hello-react"></div>
     <div id="counter"></div>
+
+    <script src="{{ asset('js/manifest.js') }}"></script>
+    <script src="{{ asset('js/vendor.js') }}"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -13,6 +13,7 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .react()
+    .extract(['react'])
     .postCss('resources/css/app.css', 'public/css', [
         //
     ]);
